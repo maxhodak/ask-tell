7d576906bcf6e8a43004c3d1250ef22b01c9bc04
Max Hodak <maxhodak@gmail.com>
2009-10-11 03:18:28 -0700

    protecting against huge entries

--- src/server.py
+++ src/server.py
@@ -7,6 +7,7 @@ port = 11911
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.allow_reuse_address = 1
 s.bind((host,port))
 s.listen(backlog)
 messages = []
@@ -16,24 +17,28 @@ while 1:
   client, address = s.accept()
   data = client.recv(size)
   if data:
-    if data == '<ask>':
-      client.send(next_msg)
-      if len(messages) > 0:
-        if len(messages) > 5:
-          denom = 5.0
-        else:
-          if float(len(messages)-1) < 1e-2:
-            denom = 1.0
-          else:
-            denom = float(len(messages)-1)
-        ind = int(round(random.expovariate(1.0/denom),0))
-        ind = len(messages)-ind
-        if ind > len(messages)-1 or ind < 0:
-          ind = len(messages)-1
-        next_msg = messages[ind]
-    else:
-      logfile.write(data+"\n")
+    if len(data) > 300:
+      logfile.write("Rejected due to length\n")
       logfile.flush()
-      messages.append(data)
-      client.send("true")
+    else:
+      if data == '<ask>':
+        client.send(next_msg)
+        if len(messages) > 0:
+          if len(messages) > 5:
+            denom = 5.0
+          else:
+            if float(len(messages)-1) < 1e-2:
+              denom = 1.0
+            else:
+              denom = float(len(messages)-1)
+          ind = int(round(random.expovariate(1.0/denom),0))
+          ind = len(messages)-ind
+          if ind > len(messages)-1 or ind < 0:
+            ind = len(messages)-1
+          next_msg = messages[ind]
+      else:
+        logfile.write(data+"\n")
+        logfile.flush()
+        messages.append(data)
+        client.send("true")
   client.close()

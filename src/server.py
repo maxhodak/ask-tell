7f0590d2b3f36a3887f7e272727d00574cd2e292
Max Hodak <maxhodak@gmail.com>
2009-10-11 04:19:16 -0700

    typo

--- src/server.py
+++ src/server.py
@@ -15,7 +15,7 @@ s.bind((host,port))
 s.listen(backlog)
 messages = []
 next_msg = "Nothing here!"
-f = open("/tmp/notspam.log",'r')
+f = open("/tmp/asktell.notspam.log",'r')
 i = 0
 for line in f:
   if i > 50:

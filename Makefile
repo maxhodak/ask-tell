4116ad6a93dd75efdae820f23f1e4d17d27f8e3d
Max Hodak <maxhodak@gmail.com>
2009-10-11 02:03:54 -0700

    initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+SHELL = /bin/sh
+build:
+install:
+	@sudo cp src/ask /usr/local/bin
+	@sudo cp src/tell /usr/local/bin
---
+++ src/server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import socket, random
+
+host = ''
+port = 11911
+backlog = 5
+size = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+messages = []
+next_msg = "Nothing here!"
+while 1:
+  client, address = s.accept()
+  data = client.recv(size)
+  if data:
+    if data == '<ask>':
+      client.send(next_msg)
+      if len(messages) > 5:
+        denom = 5.0
+      else:
+        denom = float(len(messages)-1)
+      ind = int(round(random.expovariate(1.0/denom),0))
+      ind = len(messages)-ind
+      if ind > len(messages)-1:
+        ind = len(messages)-1
+      next_msg = messages[ind]
+    else:
+      messages.append(data)
+      client.send("true")
+  client.close()
